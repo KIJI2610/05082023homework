1ef03a8d1df54e092f11c1c02860abeb28954598
Alexey <kalnitskiy2018@mail.ru>
2023-08-05 12:00:17 +0500

    ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,28 @@
+﻿int[,] matrix1 = new int[,] { { 2, 4 }, { 3, 2 } };
+int[,] matrix2 = new int[,] { { 3, 4 }, { 3, 3 } };
+int[,] result = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+for (int i = 0; i < matrix1.GetLength(0); i++)
+{
+
+    for (int j = 0; j < matrix2.GetLength(1); j++)
+    {
+    
+        int sum = 0;
+        for (int k = 0; k < matrix1.GetLength(1); k++)
+        {
+            sum += matrix1[i, k] * matrix2[k, j];
+        }
+
+        result[i, j] = sum;
+    }
+}
+
+for (int i = 0; i < result.GetLength(0); i++)
+{
+    for (int j = 0; j < result.GetLength(1); j++)
+    {
+        Console.Write(result[i, j] + " ");
+    }
+    Console.WriteLine();
+}
