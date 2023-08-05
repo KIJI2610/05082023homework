f17e5be6f178ce1eaa72535d5362c2b764df1b81
Alexey <kalnitskiy2018@mail.ru>
2023-08-05 15:00:49 +0500

    ex4

---
+++ ex4/Program.cs
@@ -0,0 +1,11 @@
+﻿int[,,] arr = new int[2,2,2];
+for(int i = 0; i < arr.GetLength(0); i++){
+    for(int j = 0; j < arr.GetLength(1); j++){
+          for(int k = 0; k < arr.GetLength(1); k++){
+              arr[i,j,k] = new Random().Next(10,100);  
+              System.Console.Write("["+arr[i,j,k]+"]"+" ");
+}
+System.Console.WriteLine();
+}
+System.Console.WriteLine();
+}
