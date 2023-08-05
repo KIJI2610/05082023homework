58733b43c1ece286016e2a6bf354de93459dcc56
Alexey <kalnitskiy2018@mail.ru>
2023-08-05 11:48:32 +0500

    ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿int[,] arr = new int[3, 4];
+for (int i = 0; i < arr.GetLength(0); i++){
+    for (int j = 0; j < arr.GetLength(1); j++){
+        arr[i, j] = new Random().Next(10, 100);
+         System.Console.Write("["+arr[i,j]+"]"+" ");
+    }
+   System.Console.WriteLine();
+}
+int[] sum = new int[3];
+for (int i = 0; i < arr.GetLength(0); i++){
+    int sumStr = 0;
+    for (int j = 0; j < arr.GetLength(1); j++){
+        sumStr += arr[i,j];     
+    }
+    sum[i] = sumStr;
+   System.Console.WriteLine();
+}
+for(int i = 0; i < sum.Length; i++){
+    System.Console.WriteLine("Сумма первой строчки: "+"["+sum[i]+"]"+" ");
+}
+System.Console.WriteLine();
+System.Console.WriteLine("Самая большая сумма строки равна: "+sum.Max());
+
